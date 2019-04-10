9c404391813879dc7eea96eee7e22ca6f31e456c
Steve Sweetney <stevesweetney@gmail.com>
2019-04-10 11:01:35 -0400

    Add error handling when checking for arguments passed to the program

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use std::rc::Rc;
 
 const iterations: u32 = 2048;
 
-type Color = (u32, u32, u32);
+const DEFAULT_ITERATIONS: u32 = 1024;
 type RcQuad = Rc<RefCell<Quad>>;
 type RcImage = Rc<RefCell<DynamicImage>>;
 
@@ -154,7 +154,16 @@ fn weighted_average(histogram: &[u32]) -> (u32, f32) {
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let image_path = &args[1];
+    let image_path = match args.get(1) {
+        Some(path) => path,
+        None => panic!("An image path must be specified"),
+    };
+
+    let iterations = match args.get(2) {
+        Some(iters) => iters.parse().unwrap(),
+        None => DEFAULT_ITERATIONS,
+    };
+
     let image =
         image::open(image_path).expect(&format!("Error opening target image {}\n", image_path));
 
