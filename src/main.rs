d22f9f0e55c61ac8e9c53e5689876b46186c0be9
Steve Sweetney <stevesweetney@gmail.com>
2019-04-29 15:26:25 -0400

    Add clap

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
+use clap::{App, Arg};
 use image::{self, imageops, GenericImageView};
-use std::env;
 
 use basic::Model;
 
@@ -8,21 +8,42 @@ const IMAGE_BOUNDS: u32 = 256;
 const DEFAULT_ITERATIONS: u32 = 1024;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let image_path = match args.get(1) {
-        Some(path) => path,
-        None => panic!("An image path must be specified"),
-    };
+    let default_iters = DEFAULT_ITERATIONS.to_string();
+    let matches = App::new("basic")
+        .version("0.1")
+        .author("Steve S.")
+        .about("Making images simpler")
+        .arg(
+            Arg::with_name("input")
+                .short("i")
+                .long("input")
+                .value_name("FILE")
+                .help("Path to input image")
+                .takes_value(true)
+                .required(true),
+        )
+        .arg(
+            Arg::with_name("iterations")
+                .long("iters")
+                .help("Set the number of times the algorithm will run")
+                .value_name("NUM")
+                .takes_value(true)
+                .default_value(&default_iters),
+        )
+        .arg(
+            Arg::with_name("padding")
+                .short("p")
+                .long("padding")
+                .help("Add padding to quadrants"),
+        )
+        .get_matches();
 
-    let iterations = match args.get(2) {
-        Some(iters) => iters.parse().unwrap(),
-        None => DEFAULT_ITERATIONS,
-    };
-
-    let pad = match args.get(3) {
-        Some(pad) => pad.parse().unwrap(),
-        None => false,
-    };
+    let image_path = matches.value_of("input").unwrap();
+    let iterations = matches
+        .value_of("iterations")
+        .and_then(|iter| iter.parse().ok())
+        .unwrap();
+    let pad = matches.is_present("padding");
 
     let image = image::open(image_path)
         .unwrap_or_else(|_| panic!("Error opening target image {}\n", image_path));
